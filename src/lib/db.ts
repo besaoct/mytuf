df9524353ca50f80de6bbe7936a421596b7a09d5
Shafin <blackipie@outlook.com>
2024-08-13 02:47:32 +0530

    v1

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -7,7 +7,7 @@ export const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10000,
+  connectionLimit: 100,
   queueLimit: 0
 });
 
